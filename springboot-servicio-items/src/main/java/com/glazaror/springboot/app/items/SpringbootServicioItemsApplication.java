74a2f58a3ff3e6070754de5960b9e298d3c2b90a
Gerardo Lázaro <glazaror@gmail.com>
2019-08-13 20:22:08 -0500

    Proyecto de referencia Spring Cloud - servicios cliente Eureka

--- springboot-servicio-items/src/main/java/com/glazaror/springboot/app/items/SpringbootServicioItemsApplication.java
+++ springboot-servicio-items/src/main/java/com/glazaror/springboot/app/items/SpringbootServicioItemsApplication.java
@@ -2,10 +2,16 @@ package com.glazaror.springboot.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
+// EnableEurekaClient es habilitado de forma implicita al declarar dependencia en el build.gradle
+// sin embargo como buena practica es mejor declarar de forma explicita
+@EnableEurekaClient
+// Con Eureka ya no se necesita especificar el nombre del servicio con ribbon... viene implicito con Eureka
+// @RibbonClient(name="servicio-productos")
+// Feign si es necesario, por lo que lo vamos a usar como cliente para conectarnos a la API REST
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
--- springboot-servicio-productos/src/main/java/com/glazaror/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/glazaror/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,11 @@ package com.glazaror.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//EnableEurekaClient es habilitado de forma implicita al declarar dependencia en el build.gradle
+//sin embargo como buena practica es mejor declarar de forma explicita
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
